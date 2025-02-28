a29256636bf50ff40eb2384dc349a52321b38637
yuduo <894529808@qq.com>
2025-02-28 16:02:15 +0800

    chore: config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      // emitFile: true,
+      // filename: "stats.html",
+      open: true, // 打包后自动打开页面
+      gzipSize: true, // 查看 gzip 压缩大小
+      brotliSize: true, // 查看 brotli 压缩大小
+    }),
+  ],
 });
